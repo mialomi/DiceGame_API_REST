e87138595067f7d45723ba61362e85254b4f1296
Mialomi <e.silvasm@gmail.com>
2023-10-03 12:36:49 +0200

    First part for login request

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    
 
-    
+    // user login
+    public function login(Request $request){
+
+        $user_login = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+
+        ]);
+        //si les credencials estan malament
+
+        if ( !Auth::attempt($user_login)){
+            return response(['message' => 'Invalid login credentials']);
+        }
+
+
+
+
+    }
+
+
 }
