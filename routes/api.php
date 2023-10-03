0183912f6da187bb2a913e3a969d90c6b296bceb
Mialomi <e.silvasm@gmail.com>
2023-10-03 10:58:17 +0200

    creation of usercontroller

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return "Hello World!";
+  });
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
