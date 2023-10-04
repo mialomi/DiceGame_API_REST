e3c7a12d5355f31b231742a601fca7d6dc2d9854
Mialomi <e.silvasm@gmail.com>
2023-10-04 18:52:22 +0200

    register controller done

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,11 +7,38 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\PersonalAccessTokenResult;
 use App\Models\User;
-
-
+use GuzzleHttp\Psr7\Message;
 
 class UserController extends Controller
 {
+
+//register
+
+    public function register(Request $request){
+
+        //validamos los datos
+        $user_data = $request->validate ([
+            'nickname' => 'nullable|string',
+            'email' => 'required|string',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        //creamos nuevo usuario 
+
+        $user = User::create([
+            'nickname' => $user_data['nickname'],
+            'email' => $user_data['email'],
+            'password' =>bcrypt($user_data['password'])
+        ]);
+
+        if (User::create($user)) {
+            return response()->json([
+                'message' => 'Successfully created',
+            ], 200);
+
+        }
+
+    }
 
     // user login
     public function login(Request $request){
