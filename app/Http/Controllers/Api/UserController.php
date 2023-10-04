fb8c18a598e1f3eb45ea7f2742a6eac3a7968967
Mialomi <e.silvasm@gmail.com>
2023-10-04 18:02:57 +0200

    login controller done

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\PersonalAccessTokenResult;
+use App\Models\User;
+
+
 
 class UserController extends Controller
 {
@@ -12,19 +16,32 @@ class UserController extends Controller
     // user login
     public function login(Request $request){
 
-        $user_login = $request->validate([
+        $user_login = $request->validate ([
             'email' => 'required|string',
             'password' => 'required|string',
 
         ]);
-        //si les credencials estan malament
+        //revisa las credenciales. Si son correctas:
 
-        if ( !Auth::attempt($user_login)){
-            return response(['message' => 'Invalid login credentials']);
+        if(Auth::attempt($user_login)) {
+
+            $user = Auth::user();
+            $token = $user->createToken('user_token')->accessToken;
+
+            return response()->json ([
+                'user' => $user,
+                'message' => 'Successfully logged in',
+                'access_token' => $token,
+
+            ], 200);
         }
-
-
-
+        
+        //si no son correctas
+        else {
+            return response()->json
+            (['message' => 'Invalid login credentials'
+            ], 401);
+        }
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
