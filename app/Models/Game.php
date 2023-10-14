8a714abf7ef792a8bcaf31143dc3f3c514cb54e2
Mialomi <e.silvasm@gmail.com>
2023-10-14 18:46:56 +0200

    adds to model game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -10,7 +10,11 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
-
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = ['dice1', 'dice2', 'result', 'user_id'];
 
 
